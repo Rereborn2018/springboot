9980cc18acd92035165103cef77da4ef7fb36e59
Rerebron2018 <sdzs853884073>
2019-12-13 10:18:47 +0800

    first springbot

---
+++ src/main/java/org/ruaho/BilibiliApplication.java
@@ -0,0 +1,28 @@
+package org.ruaho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@RestController
+public class BilibiliApplication implements WebMvcConfigurer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilibiliApplication.class, args);
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "第一个springboot项目";
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+//        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+}
---
+++ src/main/java/org/ruaho/controller/LogController.java
@@ -0,0 +1,8 @@
+package org.ruaho.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public class LogController {
+
+}
